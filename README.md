a90cad305bf61d4b67d6434f6be10b04d6c04818
Eunchurn Park <eunchurn.park@gmail.com>
2022-12-23 14:12:31 +0900

    update docs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# threejs-transform-controls
-Created with CodeSandbox
+# Three.js Transform Controls
+
+Combining TransformControls, OrbitControls and Valtio.
+
+![thumbnail](thumbnail.png)
+
+[CodeSandbox](https://codesandbox.io/s/transformcontrols-and-makedefault-forked-80yiv8)
+
+[Demo](https://eunchurn.github.io/threejs-transform-controls)
--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import { Suspense, useState } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, TransformControls, ContactShadows, useGLTF, useCursor } from '@react-three/drei'
-import { proxy, useSnapshot } from 'valtio'
+import { Suspense, useState } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, TransformControls, ContactShadows, useGLTF, useCursor } from "@react-three/drei";
+import { proxy, useSnapshot } from "valtio";
 
 // Reactive state model, using Valtio ...
-const modes = ['translate', 'rotate', 'scale']
-const state = proxy({ current: null, mode: 0 })
+const modes = ["translate", "rotate", "scale"];
+const state = proxy({ current: null, mode: 0 });
 
 function Model({ name, ...props }) {
   // Ties this component to the state model
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
   // Fetching the GLTF, nodes is a collection of all the meshes
   // It's cached/memoized, it only gets loaded and parsed once
-  const { nodes } = useGLTF('/compressed.glb')
+  const { nodes } = useGLTF("/compressed.glb");
   // Feed hover state into useCursor, which sets document.body.style.cursor to pointer|auto
-  const [hovered, setHovered] = useState(false)
-  useCursor(hovered)
+  const [hovered, setHovered] = useState(false);
+  useCursor(hovered);
   return (
     <mesh
       // Click sets the mesh as the new target
+      // eslint-disable-next-line no-sequences
       onClick={(e) => (e.stopPropagation(), (state.current = name))}
       // If a click happened but this mesh wasn't hit we null out the target,
       // This works because missed pointers fire before the actual hits
-      onPointerMissed={(e) => e.type === 'click' && (state.current = null)}
+      onPointerMissed={(e) => e.type === "click" && (state.current = null)}
       // Right click cycles through the transform modes
       onContextMenu={(e) => snap.current === name && (e.stopPropagation(), (state.mode = (snap.mode + 1) % modes.length))}
+      // eslint-disable-next-line no-sequences
       onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}
       onPointerOut={(e) => setHovered(false)}
       name={name}
       geometry={nodes[name].geometry}
       material={nodes[name].material}
-      material-color={snap.current === name ? '#ff6080' : 'white'}
+      material-color={snap.current === name ? "#ff6080" : "white"}
       {...props}
       dispose={null}
     />
-  )
+  );
 }
 
 function Controls() {
   // Get notified on changes to state
-  const snap = useSnapshot(state)
-  const scene = useThree((state) => state.scene)
+  const snap = useSnapshot(state);
+  const scene = useThree((state) => state.scene);
   return (
     <>
       {/* As of drei@7.13 transform-controls can refer to the target by children, or the object prop */}
@@ -48,7 +50,7 @@ function Controls() {
       {/* makeDefault makes the controls known to r3f, now transform-controls can auto-disable them when active */}
       <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
     </>
-  )
+  );
 }
 
 export default function App() {
@@ -72,5 +74,5 @@ export default function App() {
       </Suspense>
       <Controls />
     </Canvas>
-  )
+  );
 }
